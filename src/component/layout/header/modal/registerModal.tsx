250fa9f440ebf81ee94979bfa07e80d75150b0db
ninano <hiru2128@gmail.com>
2023-06-28 00:46:20 +0900

    Feat : click 버튼을 누르면 요청 URL

--- src/component/layout/header/modal/registerModal.tsx
+++ src/component/layout/header/modal/registerModal.tsx
@@ -5,6 +5,7 @@ import kakaoIc from "@/img/ic_kakao.png";
 import Link from "next/link";
 import { ModalBox, ModalContent } from "./modal.style";
 import {getCsrfToken, signIn, useSession} from "next-auth/react";
+import axios from "axios";
 
 interface clickModalType {
   closeRegisterModal: any;
@@ -17,6 +18,13 @@ const RegisterModal = ({ closeRegisterModal, changeModal }: clickModalType) => {
     const csrfToken = await getCsrfToken();
     console.log(csrfToken);
   }
+
+  const onClickKakao = async () => {
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/signin/:kakao`);
+    } catch (e: any) {
+    }
+  };
 
   return (
     <ModalBox>
@@ -42,7 +50,21 @@ const RegisterModal = ({ closeRegisterModal, changeModal }: clickModalType) => {
               />
               <div className="googleText">구글로 시작하기</div>
             </button>
+            {/*<Link*/}
+            {/*  className="google"*/}
             {/*  href={process.env.NEXT_PUBLIC_API_URL + "/auth/google"}*/}
+            {/*>*/}
+            {/*  <Image*/}
+            {/*    src={googleIc}*/}
+            {/*    className="googleImage"*/}
+            {/*    alt="구글"*/}
+            {/*    width={18}*/}
+            {/*    height={18}*/}
+            {/*  />*/}
+            {/*  <div className="googleText">구글로 시작하기</div>*/}
+            {/*</Link>*/}
+            {/*<Link*/}
+            {/*  className="kakao"*/}
             {/*  href={process.env.NEXT_PUBLIC_API_URL + "/auth/kakao"}*/}
             <button className="kakao" onClick={() => signIn("kakao")}>
               <Image
@@ -54,7 +76,8 @@ const RegisterModal = ({ closeRegisterModal, changeModal }: clickModalType) => {
               />
               <div className="kakaoText">카카오로 시작하기</div>
             </button>
-            <button onClick={getSocialLoginToken}>click</button>
+            <button onClick={onClickKakao}>click</button>
+            {/*</Link>*/}
             <div className="register" onClick={changeModal}>
               <Link href="">이미 회원이신가요?</Link>
             </div>
