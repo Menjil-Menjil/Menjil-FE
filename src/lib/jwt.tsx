e2196edea3530392a9496c6a6d32384a78dae167
ninano <hiru2128@gmail.com>
2023-07-25 17:36:18 +0900

    refactor: 사용하지 않는코드 정리

--- src/lib/jwt.tsx
+++ src/lib/jwt.tsx
@@ -1,16 +1,6 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
 import axios from "axios";
 
-interface SignOption {
-  algorithm?: any;
-  expiresIn?: string | number;
-}
-
-const DEFAULT_SIGN_OPTION: SignOption = {
-  algorithm: "HS512",
-  expiresIn: "3m",
-};
-
 export const authedTokenAxios = (accessToken: any, refreshToken: any) => {
   return axios.create({
     // header를 여기서 설정해주면,
@@ -19,31 +9,6 @@ export const authedTokenAxios = (accessToken: any, refreshToken: any) => {
       Authorization: `Bearer ${accessToken} ${refreshToken}`
     }
   })
-}
-
-export function signJwtAccessToken(payload: JwtPayload, options: SignOption = DEFAULT_SIGN_OPTION) {
-  // const header = {
-  //   "typ": "JWT",
-  //   "alg": "HS512"
-  // };
-  // const encodedHeader = new Buffer(JSON.stringify(header))
-  //   .toString('base64')
-  //   .replaceAll('=', '');
-  // const encodedPayload = new Buffer(JSON.stringify(payload))
-  //   .toString('base64')
-  //   .replaceAll('=', '');
-  // const crypto = require('crypto');
-  // const signature = crypto
-  //   .createHmac('sha512', process.env.TOKEN_SECRET_KEY)
-  //   .update(`${encodedHeader}.${encodedPayload}`)
-  //   .digest('base64')
-  //   .replaceAll('=', '');
-
-  //return `${encodedHeader}.${encodedPayload}.${signature}`
-  require('dotenv').config();
-  const jwt = require("jsonwebtoken");
-  const secret_key = process.env.TOKEN_SECRET_KEY;
-  return jwt.sign(payload, secret_key!, options);
 }
 
 export function verifyJwt(token: string) {
