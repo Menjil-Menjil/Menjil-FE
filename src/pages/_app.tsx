82dcb3d54248b01092ae82462dbc42945bd773b8
ninano <hiru2128@gmail.com>
2023-08-15 14:40:17 +0900

    feat: 글로벌 폰트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import Layout from "@/component/layout";
 import { Prompt } from "next/font/google";
 import localFont from 'next/font/local';
 import React from "react";
-import  "../styles/globals.css";
+//import  "../styles/globals.css";
 import {SessionProvider} from "next-auth/react";
 import AuthContainer from "@/authContainer";
 
@@ -14,7 +14,7 @@ const prompt = Prompt({
   variable: "--prompt",
 });
 const pretendard = localFont({
-  src: '../../public/fonts/pretendard/Pretendard-Regular.woff',
+  src: '../../public/fonts/pretendard/Pretendard-Medium.woff',
 });
 
 export interface AuthInfo {
@@ -77,19 +77,71 @@ export default function App(
           {renderAuthorizedComponent()}
         </Layout>
         <style jsx global>{`
-            :root {
-              /* font */
-              --logo-font: ${prompt.style.fontFamily};
-              /* color */
-              --theme-color: #e68c23;
-              --input-placeholder: #afafaf;
-              --highlighted-element: #ff8a00;
-              --selected-element: #fbbc053b;
-            }
-            html, 
-            body {
-              font-family: ${pretendard.style.fontFamily}, "Pretendard", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-            }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Black.woff");
+            font-weight: 900;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-ExtraBold.woff");
+            font-weight: 800;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Bold.woff");
+            font-weight: 700;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-SemiBold.woff");
+            font-weight: 600;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Medium.woff");
+            font-weight: 500;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Regular.woff");
+            font-weight: 400;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Light.woff");
+            font-weight: 300;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-ExtraLight.woff");
+            font-weight: 200;
+          }
+          @font-face {
+            font-family: "Pretendard";
+            src: local("/public/fonts/pretendard/Pretendard-Thin.woff");
+            font-weight: 100;
+          }
+          :root {
+            /* font */
+            --logo-font: ${prompt.style.fontFamily};
+            /* color */
+            --theme-color: #e68c23;
+            --input-placeholder: #afafaf;
+            --highlighted-element: #ff8a00;
+            --selected-element: #fbbc053b;
+          }
+          html, 
+          body {
+            font-family: 
+            "Pretendard", Pretendard,
+            -apple-system, BlinkMacSystemFont,
+            system-ui, Roboto, "Helvetica Neue",
+            "Segoe UI", "Apple SD Gothic Neo",
+            "Noto Sans KR", "Malgun Gothic",
+            "Apple Color Emoji", "Segoe UI Emoji",
+            "Segoe UI Symbol", sans-serif;
+          }
         `}</style>
       </SessionProvider>
     </>
