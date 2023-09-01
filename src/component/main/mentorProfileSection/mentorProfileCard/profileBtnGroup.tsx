2683a1d5902e09efe2a6dfd5b13813576f651a7b
ninano <hiru2128@gmail.com>
2023-09-01 18:31:06 +0900

    feat: 팔로우기능 사전 구현

--- src/component/main/mentorProfileSection/mentorProfileCard/profileBtnGroup.tsx
+++ src/component/main/mentorProfileSection/mentorProfileCard/profileBtnGroup.tsx
@@ -1,9 +1,30 @@
 import {ProfileBtnContainerDiv} from "@/component/main/mentorProfileSection/profileCard.style";
 import IcQuestion from "@/img/ic_send-question.svg";
 import IcFollow from "@/img/ic_follow.svg";
-const ProfileBtnGroup = () => {
-  const onClickFollow = (e: any) => {
-    console.log(e.target.value)
+import {useRecoilValue} from "recoil";
+import {userState} from "@/states/state";
+import {authedTokenAxios, refreshTokenAPI} from "@/lib/jwt";
+import {useSession} from "next-auth/react";
+
+interface propsType {
+  nickname: string,
+}
+const ProfileBtnGroup = ({nickname}: propsType) => {
+  const user = useRecoilValue(userState);
+  const mentorNickname = nickname;
+  const {data: sessionData, update: sessionUpdate} =useSession();
+  const followMentorAxios = async (sessionData: any, userName: string, mentorName: string) => {
+    try {
+      await authedTokenAxios(sessionData.accessToken)
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/api/main/follow?nickname=${userName}`)
+    } catch (error: any) {
+      console.log(`${error.response?.data?.code}: ${error.response?.data?.message}`)
+      refreshTokenAPI(sessionData, sessionUpdate).then()
+    }
+  };
+  const onClickFollow = () => {
+    //followMentorAxios(sessionData, user.name!, mentorNickname).then();
+    console.log(user.name, mentorNickname)
   };
 
   return (
@@ -11,8 +32,8 @@ const ProfileBtnGroup = () => {
       <div className="btnQuestion">
         <div className="icBoxQuestion"><IcQuestion/></div>질문하기
       </div>
-      <div className="btnFollow">
-        <div className="icBoxFollow" onClick={onClickFollow}><IcFollow/></div>팔로우하기
+      <div className="btnFollow" onClick={() => onClickFollow()}>
+        <div className="icBoxFollow"><IcFollow/></div>팔로우하기
       </div>
     </ProfileBtnContainerDiv>
   );
