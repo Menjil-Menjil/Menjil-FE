756eba1944f385bbf6c7c5570968b24f4abac2f3
ninano <hiru2128@gmail.com>
2023-09-04 18:50:58 +0900

    feat: 위로가기 버튼 수정

--- src/component/topBtn/index.tsx
+++ src/component/topBtn/index.tsx
@@ -1,8 +1,6 @@
 //참고: https://velog.io/@leemember/React-%EB%94%B0%EB%9D%BC%EB%8B%A4%EB%8B%88%EB%8A%94-Top-%EB%B2%84%ED%8A%BC-scroll-to-top-%EB%A7%8C%EB%93%A4%EA%B8%B0
 import {useEffect, useState} from "react";
 import styled from "@emotion/styled";
-import ic_top from "@/img/ic_edit.png"
-import Image from "next/image";
 
 export const TopButtonDiv = styled.div`
   position: fixed;
