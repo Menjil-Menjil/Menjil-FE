2601aefb78cc04511fa74d7b4c1af0dbaa6f6579
Ye-kki <yechan_choi@naver.com>
2023-10-09 18:03:37 +0900

    feat: stomp 전역 설정 및 기타 데이터 처리

---
+++ src/component/layout/stomp/index.tsx
@@ -0,0 +1,163 @@
+import {
+  aiQuestionState,
+  chatMessagesState,
+  nowSubscribeState,
+  pubMessageState,
+  subscribeState,
+  ISubscribe,
+} from "@/states/stateSubscribe";
+import { userState } from "@/states/stateUser";
+import { Client } from "@stomp/stompjs";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+
+var client: Client | null = null;
+
+const Stomp = () => {
+  const [subscriptionList, setSubscription] = useState<any[]>([]);
+  const [chattingRooms, setChattingRooms] = useRecoilState(subscribeState);
+  const [messagesLog, setMessagesLog] = useRecoilState(chatMessagesState); //메세지들
+  const [chattingMentor, setChattingMentor] = useRecoilState(nowSubscribeState);
+  const [messageInput, setMessageInput] = useRecoilState(pubMessageState); //보내는 메세지
+  const [aiQuestion, setAiQuestion] = useRecoilState(aiQuestionState); //보내는 메세지
+  const user = useRecoilValue(userState);
+
+  const connect = () => {
+    (client = new Client({
+      brokerURL: `${process.env.NEXT_PUBLIC_STOMP_API_URL}/ws/chat/websocket`,
+      debug: function (str) {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+    })),
+      client.activate();
+  };
+
+  const disConnect = () => {
+    if (client != null) {
+      if (client.connected) client.deactivate();
+    }
+  };
+  const bringAllChatRoom = useCallback(async () => {
+    try {
+      const res = await axios
+        .get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/chat/rooms?nickname=${
+            user.name
+          }&type=${"MENTEE"}`
+        )
+        .then((res) => {
+          setChattingRooms(() => res.data.data);
+        });
+    } catch (e: any) {
+      alert(e);
+    }
+  }, [setChattingRooms, user.name]);
+
+  // 채팅방 구독하기
+  const subscribe = (roomId: any) => {
+    const subscription = client?.subscribe(
+      `/queue/chat/room/${roomId}`,
+      ({ body }) => {
+        if (typeof JSON.parse(body).data.length === "number") {
+          //채팅이 여러 개일 경우
+          JSON.parse(body).data.map((message: any) => {
+            setMessagesLog((messagesLog) => [...messagesLog, message]);
+          });
+        } else {
+          //하나씩 오는 대화 아래에 표시
+          setMessagesLog((messagesLog) => [
+            JSON.parse(body).data,
+            ...messagesLog,
+          ]);
+        }
+      }
+    );
+    setSubscription((subscriptionList) => [...subscriptionList, subscription]);
+    return;
+  };
+
+  const publish = (message: any, messageType: string, senderType: string) => {
+    if (!client?.connected) return;
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = ("0" + (today.getMonth() + 1)).slice(-2);
+    const day = ("0" + today.getDate()).slice(-2);
+    const hours = ("0" + today.getHours()).slice(-2);
+    const minutes = ("0" + today.getMinutes()).slice(-2);
+    const seconds = ("0" + today.getSeconds()).slice(-2);
+
+    const dateString = year + "-" + month + "-" + day;
+    const timeString = hours + ":" + minutes + ":" + seconds;
+    const nowTime = dateString + " " + timeString;
+
+    client?.publish({
+      destination: `/pub/chat/room/${chattingMentor.roomId}`,
+      body: JSON.stringify({
+        roomId: chattingMentor.roomId,
+        senderType: senderType,
+        senderNickname: user.name,
+        message: message,
+        messageType: messageType,
+        time: nowTime,
+      }),
+    });
+
+    setMessageInput("");
+  };
+
+  const showQuestion = (index: any, AI_SUMMARY: string, AI_ANSWER: string) => {
+    console.log(index + "번 대화");
+    publish(index + "번 대화", "SELECT", "MENTEE");
+    setTimeout(() => {
+      publish(AI_SUMMARY, "AI_SUMMARY", "MENTOR");
+    }, 100);
+    setTimeout(() => {
+      publish(AI_ANSWER, "AI_ANSWER", "MENTOR");
+    }, 200);
+  };
+
+  useEffect(() => {
+    connect();
+    return () => disConnect();
+  }, []);
+
+  useEffect(() => {
+    bringAllChatRoom();
+  }, [bringAllChatRoom]);
+
+  useEffect(() => {
+    subscriptionList.map((_subscriptionList) =>
+      client?.unsubscribe(_subscriptionList.id)
+    );
+    setSubscription([]);
+    if (client?.connected) {
+      chattingRooms.map((_chattingRooms: any) => {
+        subscribe(_chattingRooms.roomId);
+      });
+    }
+  }, [chattingRooms]);
+
+  //메세지 보내기 (질문하기)
+  useEffect(() => {
+    if (messageInput !== "") publish(messageInput, "C_QUESTION", "MENTEE");
+    setMessageInput("");
+  }, [messageInput]);
+
+  //ai 질문 클릭
+  useEffect(() => {
+    if (aiQuestion.AI_SUMMARY !== "")
+      showQuestion(
+        aiQuestion.index,
+        aiQuestion.AI_SUMMARY,
+        aiQuestion.AI_ANSWER
+      );
+  }, [aiQuestion]);
+
+  return <></>;
+};
+
+export default Stomp;
--- src/component/main/mentorProfileSection/mentorProfileCard/index.tsx
+++ src/component/main/mentorProfileSection/mentorProfileCard/index.tsx
@@ -1,23 +1,25 @@
 import ProfileContent from "@/component/main/mentorProfileSection/mentorProfileCard/profileContent";
 import ProfileRecentQuestion from "@/component/main/mentorProfileSection/mentorProfileCard/profileRecentQuestion";
 import ProfileBtnGroup from "@/component/main/mentorProfileSection/mentorProfileCard/profileBtnGroup";
-import {ProfileCardDiv} from "@/component/main/mentorProfileSection/profileCard.style";
+import { ProfileCardDiv } from "@/component/main/mentorProfileSection/profileCard.style";
 
 interface propsType {
-  data: any,
+  data: any;
 }
-const MentorProfileCard = ({data}: propsType) => {
+const MentorProfileCard = ({ data }: propsType) => {
   return (
     <ProfileCardDiv>
-      <ProfileContent nickname={data.nickname}
-                      major={data.major}
-                      company={data.company}
-                      field={data.field}
-                      techStack={data.techStack}
-                      imgUrl={data.imgUrl}/>
-      <ProfileRecentQuestion lastAnswerList={data.lastAnsweredMessages}/>
-      <ProfileBtnGroup nickname={data.nickname}/>
+      <ProfileContent
+        nickname={data.nickname}
+        major={data.major}
+        company={data.company}
+        field={data.field}
+        techStack={data.techStack}
+        imgUrl={data.imgUrl}
+      />
+      <ProfileRecentQuestion lastAnswerList={data.lastAnsweredMessages} />
+      <ProfileBtnGroup nickname={data.nickname} imgUrl={data.imgUrl} />
     </ProfileCardDiv>
   );
-}
+};
 export default MentorProfileCard;
